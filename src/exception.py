93d016e987ddbdaa19dfb003259c14674e0b2328
PIYUS1507 <piyushrajurkar205@gmail.com>
2025-07-02 22:23:01 +0530

    this is exception and logger file

---
+++ src/exception.py
@@ -0,0 +1,26 @@
+import sys
+from logger import logger
+
+def error_message_details(error,error_details:sys):
+    _,_,exc_tb=error_details.exc_info()
+    file_name =exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error ocurred in python script name [{0}] on line no. [{1}] error messgage [{2}]".format(file_name,exc_tb.tb_lineno,str(error))
+
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_details:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message,error_details=error_details)
+    def __str__(self):
+        return self.error_message
+    
+
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+
+        logger.info("the exception has been created")
+        raise CustomException(e,sys)
